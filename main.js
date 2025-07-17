9301d26004772bb6077b8e303e6ada322af98298
JB <198005711+baumjessi@users.noreply.github.com>
2025-07-17 12:33:50 -0500

    Create basic playRound() function
    Create the skeleton of a function that lets each player choose
    coordinates on the board, changes the coordinates to the player's mark,
    and determines whose turn is next. Currently changes all coordinates
    to the player's mark instead of just one - will fix.

--- main.js
+++ main.js
@@ -1,25 +1,38 @@
-function Gameboard() {
-   var board = [
+var board = [
     ["A1", "A2", "A3"],
     ["B1", "B2", "B3"],
     ["C1", "C2", "C3"],
    ]
-   console.log(board);
-}
 
-Gameboard();
-
-function createPlayers() {
-    const playerOne = {
-        name: "Player One",
-        mark: "X"
+function createPlayers(name, mark) {
+    return {
+        name: name,
+        mark: mark
     }
-    const playerTwo = {
-        name: "Player Two",
-        mark: "O"
-    }
-    console.log(playerOne);
-    console.log(playerTwo);
 };
 
-createPlayers();
+let playerOne = createPlayers("playerOne", "x");
+let playerTwo = createPlayers("playerTwo", "x");
+
+function playRound() {
+    let currentPlayer = playerOne;
+    console.log(currentPlayer);
+    let playerOneChoice = window.prompt("player one enter coordinates");
+    console.log(`player one chose ${playerOneChoice}`);
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < 3; j++) {
+        if (playerOneChoice.toUppercase = board[i][j]) {
+            board[i][j] = "X";
+        }
+        else {
+            window.alert("oops! not a choice");
+        }
+        }
+    }
+    currentPlayer = playerTwo;
+    let playerTwoChoice = window.prompt("player two enter coordinates");
+    console.log(`player two chose ${playerTwoChoice}`);
+}
+
+playRound();
+
