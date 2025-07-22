23448bfe55ada9d862384d3fe092763c18cc4aa0
JB <198005711+baumjessi@users.noreply.github.com>
2025-07-22 11:55:53 -0500

    Rewrite all JS logic for console only
    Previously the code became too convoluted and DOM dependent. Rewrite
    all code to be cleaner, run completely from console first, and wrap
    all variables in constructors so there is nothing in the global scope.

--- main.js
+++ main.js
@@ -1,192 +1,23 @@
-var board = [
-    ["A1", "A2", "A3"],
-    ["B1", "B2", "B3"],
-    ["C1", "C2", "C3"],
-   ]
+const { cloneElement } = require("react");
 
-console.log(board);
+function Gameboard () {
+    const board = [
+        ["A1", "A2", "A3"],
+        ["B1", "B2", "B3"],
+        ["C1", "C2", "C3"]
+    ]
 
-function createPlayers(name, mark) {
-    return {
-        name: name,
-        mark: mark,
-    }
-};
+    const getBoard = () => board;
 
-let playerOne = createPlayers("playerOne", "X");
-let playerTwo = createPlayers("playerTwo", "O");
-let winningPlayer;
-
-function playRound() {
-    let currentPlayer = playerOne;
-
-    // let cells = document.querySelectorAll(".cell");
-    // let cellArray = Array.from(cells);
-    // console.log(currentPlayer);
-    // cellArray.forEach(cell => {
-    //     cell.addEventListener("click", e => {
-    //         if (currentPlayer === playerOne) {
-    //             cell.textContent = "X"; 
-    //             currentPlayer = playerTwo;
-    //         }
-    //         else if (currentPlayer === playerTwo) {
-    //             cell.textContent = "O";
-    //             currentPlayer = playerOne;
-    //         }
-    // })});
-
-    let playerOneChoice = window.prompt("player one enter coordinates");
-    let playerOneChoiceUpCase = playerOneChoice.toUpperCase();
-    for (let i = 0; i < board.length; i++) {
-        for (let j = 0; j < 3; j++) {
-            if (board[i][j] == "X" || board[i][j] == "O") { 
+    const markCell = (coordinate, player) => {
+        for (let i = 0; i < board.length; i++) {
+            for (let j = 0; j < board[i].length; j++)  {
+                if (board[i][j] === coordinate) {
+                    board[i][j].changeCell(player);
+                }
+                else {
+                    return;
+                }
             }
-            if (board[i][j] === playerOneChoiceUpCase) {
-                board[i][j] = "X";
-                console.log (`player one chose ${playerOneChoice}!`)
             }
-        }
-    }
-    currentPlayer = playerTwo;
-    let playerTwoChoice = window.prompt("player two enter coordinates");
-    let playerTwoChoiceUpCase = playerTwoChoice.toUpperCase();
-    for (let i = 0; i < board.length; i++) {
-        for (let j = 0; j < 3; j++) {
-            if (board[i][j].includes(playerTwoChoiceUpCase)) {
-                board[i][j] = "O";
-                console.log (`player two chose ${playerTwoChoice}!`)
-            }
-        }
-    }
-    console.log(board);
-}
-
-function checkBoard() {
-    for (let i = 0; i < board.length; i++) {
-    for (let j = 0; j < 3; j++) {
-        if (board[0][0] == "X" && 
-            board[0][1] == "X" &&
-            board[0][2] == "X") {
-                return winningPlayer = playerOne;
-            }
-        else if (board[0][0] == "O" && 
-            board[0][1] == "O" &&
-            board[0][2] == "O") {
-                return winningPlayer = playerTwo;
-            }
-        else if (board[1][0] == "X" && 
-            board[1][1] == "X" &&
-            board[1][2] == "X") {
-                return winningPlayer = playerOne;
-            }
-        else if (board[1][0] == "O" && 
-            board[1][1] == "O" &&
-            board[1][2] == "O") {
-                return winningPlayer = playerTwo;
-            }
-        else if (board[2][0] == "X" && 
-            board[2][1] == "X" &&
-            board[2][2] == "X") {
-                return winningPlayer = playerOne;
-            }
-        else if (board[2][0] == "O" && 
-            board[2][1] == "O" &&
-            board[2][2] == "O") {
-                return winningPlayer = playerTwo;
-            }
-        else if (board[0][0] == "X" && 
-            board[1][0] == "X" &&
-            board[2][0] == "X") {
-                return winningPlayer = playerOne;
-            }
-        else if (board[0][0] == "O" && 
-            board[1][0] == "O" &&
-            board[2][0] == "O") {
-                return winningPlayer = playerTwo;
-            }
-        else if (board[0][1] == "X" && 
-            board[1][1] == "X" &&
-            board[2][1] == "X") {
-                return winningPlayer = playerOne;
-            }
-        else if (board[0][1] == "O" && 
-            board[1][1] == "O" &&
-            board[2][1] == "O") {
-                return winningPlayer = playerTwo;
-            }
-        else if (board[0][2] == "X" && 
-            board[1][2] == "X" &&
-            board[2][2] == "X") {
-                return winningPlayer = playerOne;
-            }
-        else if (board[0][2] == "O" && 
-            board[1][2] == "O" &&
-            board[2][2] == "O") {
-                return winningPlayer = playerTwo;
-            }
-        else if (board[0][0] == "X" && 
-            board[1][1] == "X" &&
-            board[2][2] == "X") {
-                return winningPlayer = playerOne;
-            }
-        else if (board[0][0] == "O" && 
-            board[1][1] == "O" &&
-            board[2][2] == "O") {
-                return winningPlayer = playerTwo;
-            }
-        else if (board[0][2] == "X" && 
-            board[1][1] == "X" &&
-            board[2][0] == "X") {
-                return winningPlayer = playerOne;
-            }
-        else if (board[0][2] == "O" && 
-            board[1][1] == "O" &&
-            board[2][0] == "O") {
-                return winningPlayer = playerTwo;
-            }
-        else {
-            playRound();
-        }
-}
-}
-};
-
-function declareWinner() {
-    if (winningPlayer === playerOne) {
-        console.log("player one wins!");
-    }
-    else if (winningPlayer === playerTwo) {
-        console.log("player two wins!")
-    };
-}
-
-function playGame() {
-    checkBoard();
-    declareWinner();
-}
-
-playGame();
-
- let currentPlayer = playerOne;
- let playerDisplay = document.getElementById("player-display");
-
-    let cells = document.querySelectorAll(".cell");
-    let cellArray = Array.from(cells);
-    console.log(currentPlayer);
-    cellArray.forEach(cell => {
-        cell.addEventListener("click", e => {
-            if (!(cell.classList.contains("taken"))) {
-            if (currentPlayer === playerOne) {
-                cell.textContent = "X"; 
-                cell.classList.add("taken");
-                currentPlayer = playerTwo;
-                playerDisplay.textContent = "player  two's turn!";
-
-            }
-            else if (currentPlayer === playerTwo) {
-                cell.textContent = "O";
-                cell.classList.add("taken");
-                currentPlayer = playerOne;
-                playerDisplay.textContent = "player one's turn!"
-            }}
-    })});
+        }}
