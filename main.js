46c9f2fdcc24d92849cd2c174863064b7b304068
JB <198005711+baumjessi@users.noreply.github.com>
2025-07-23 10:56:22 -0500

    Change values in Cell() from strings to #s
    Change values in the Cell() constructor from
    strings to numbers so that they display when the
    board is printed. Previously, each item in the
    array displayed its available functions rather
    than a value.
    
    Game is now working fully in console apart from
    logic to check for wins.

--- main.js
+++ main.js
@@ -1,23 +1,84 @@
-const { cloneElement } = require("react");
+function Gameboard() {
+  const rows = 3;
+  const columns = 3;
+  const board = [];
 
-function Gameboard () {
-    const board = [
-        ["A1", "A2", "A3"],
-        ["B1", "B2", "B3"],
-        ["C1", "C2", "C3"]
-    ]
+    for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < columns; j++) {
+      board[i].push(Cell());
+    }
+  }
 
-    const getBoard = () => board;
+  const getBoard = () => board;
 
-    const markCell = (coordinate, player) => {
-        for (let i = 0; i < board.length; i++) {
-            for (let j = 0; j < board[i].length; j++)  {
-                if (board[i][j] === coordinate) {
-                    board[i][j].changeCell(player);
-                }
-                else {
-                    return;
-                }
-            }
-            }
-        }}
+  const markCell = (row, column, player) => {
+    const availableCells = board.filter((row) => row[column].getValue() === 0).map(row => row[column]);
+    if (!availableCells.length) return;
+    const chosenCell = board[row][column];
+    chosenCell.changeCell(player);
+    }
+
+
+  const printBoard = () => {
+    const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()))
+    console.log(boardWithCellValues);
+  };
+
+  return { getBoard, markCell, printBoard };
+}
+
+function Cell() {
+    let value = 0;
+
+    const changeCell = (player) => {
+        value = player;
+    }
+
+    const getValue = () => value;
+
+    return { changeCell, getValue };
+}
+
+function GameController(
+    playerOneName = "Player One",
+    playerTwoName = "Player Two"
+) {
+    const board = Gameboard();
+
+    const players = [{
+        name: playerOneName,
+        mark: 1
+    },
+    {
+        name: playerTwoName,
+        mark: 2
+    }]
+
+    let activePlayer = players[0];
+
+    const switchPlayerTurn = () => {
+            activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    };
+
+    const getActivePlayer = () => activePlayer;
+
+    const printNewRound = () => {
+        board.printBoard();
+        console.log(`${getActivePlayer().name}'s turn!`);
+    };
+
+    const playRound = (row, column) => {
+        console.log(`${getActivePlayer().name} placed their mark!`)
+        board.markCell(row, column, getActivePlayer().mark);
+        switchPlayerTurn();
+        printNewRound();
+    }
+
+    printNewRound();
+
+    return { playRound, getActivePlayer };
+
+    }
+
+    const game = GameController();
